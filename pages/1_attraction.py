d6de0c57f6757e8a18f2b0ef662973878db8be64
meportter <meporter385@gmail.com>
2024-12-06 06:14:00 +0000

    just test

--- pages/1_attraction.py
+++ pages/1_attraction.py
@@ -1,3 +1,11 @@
 import streamlit as st
 from openai import OpenAI
 import json
+
+
+# 세션 상태에 저장된 음식점 목록 출력
+if "selected_restaurants" in st.session_state:
+    client = st.session_state.get('selected_restaurants', None)
+    st.write(client)
+else:
+    st.write("선택된 음식점이 없습니다.")
